94eaaf355686d63a22b1b238ce63d7ef96d265fb
Irshman <76690755+Irshman@users.noreply.github.com>
2022-01-28 17:53:13 +0200

    fix adaptive

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 
 let burger = document.querySelector('.header__burger');
 let nav = document.querySelector('.header__nav');
+let button = document.querySelector(".header__btn");
 
 burger.addEventListener('click', function() {
   burger.classList.toggle('header__burger--active');
   nav.classList.toggle('header__nav--active');
+  button.classList.toggle('header__btn--active');
 });
